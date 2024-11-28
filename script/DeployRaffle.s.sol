50101870a56f1128c51e75e7b936fa34a8ff2e93
SquilliamX <smartchainwizz@gmail.com>
2024-11-27 22:35:23 -0700

    Minor Changes - creates a subscription Id if one does not exist

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
@@ -15,6 +16,15 @@ contract DeployRaffle is Script {
         // its also the same as doing ` HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);`
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
+        // if the subscription id does not exist, create one
+        if (config.subscriptionId == 0) {
+            // deploys a new CreateSubscription contract from Interactions.s.sol and save it as a variable named createSubscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            // calls the createSubscription contract's createSubscription function and passes the vrfCoordinator from the networkConfigs dependent on the chain we are on. This will create a subscription for our vrfCoordinator. Then we save the return values of the subscriptionId and vrfCoordinator and vrfCoordinator as the subscriptionId and values in our networkConfig.
+            (config.subscriptionId, config.vrfCoordinator) =
+                createSubscription.createSubscription(config.vrfCoordinator);
+        }
+
         // everything between startBroadcast and stopBroadcast is broadcasted to a real chain
         vm.startBroadcast();
         // create a new raffle contract with the parameters that are in the Raffle's constructor. This HAVE to be in the same order as the constructor!
@@ -22,7 +32,7 @@ contract DeployRaffle is Script {
             // we do `config.` before each one because our helperConfig contract grabs the correct config dependent on the chain we are deploying to
             config.entranceFee,
             config.interval,
-            config.vrfCoordinatior,
+            config.vrfCoordinator,
             config.gasLane,
             config.subscriptionId,
             config.callBackGasLimit
